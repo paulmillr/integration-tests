1c19ce26471373c357332e4edc0356382051ac56
Paul Miller <paul@paulmillr.com>
2025-01-24 18:11:03 +0000

    fuzz-install

---
+++ fuzz-install.sh
@@ -0,0 +1,4 @@
+sudo apt install -y bzip2 build-essential libc++-dev libc++-abi-dev lsb-release software-properties-common gnupg libboost-all-dev xxd cmake git wget curl bc jq
+wget https://apt.llvm.org/llvh.sh
+chmod +x llvm.sh
+sudo ./llvm.sh 15
